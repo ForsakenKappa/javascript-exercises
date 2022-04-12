a110b1135b9f6d200d5f1c6a2681be3ae176c23d
ForsakenKappa <jafsadcookie@yandex.ru>
2022-04-13 08:24:26 +1000

    04 is done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,8 +1,10 @@
 const removeFromArray = function(array = [], ...itemsToRemove) {
 
     return array.filter((item) => {
-        for (let i = 0; i < itemsToRemove.length; i++)
-        return !(item === itemsToRemove[i])
+        for (let i = 0; i < itemsToRemove.length; i++){
+            if (item === itemsToRemove[i]) return false
+        }
+        return true
     })
 
 };
