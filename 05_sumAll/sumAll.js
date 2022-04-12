68b504caa09316245a83ac43a42f810d149b1885
ForsakenKappa <jafsadcookie@yandex.ru>
2022-04-13 09:07:05 +1000

    05 is done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(numStart, numEnd) {
 
+    if ( !(typeof numEnd === 'number') || !(typeof numStart === 'number')) return "ERROR"
+    if ( numEnd < 0 || numStart < 0) return "ERROR"
+
+    if (numStart > numEnd) {
+        a = numEnd;
+        b = numStart;
+    }
+    else{
+        a = numStart;
+        b = numEnd;
+    }
+
+    sum = 0;
+
+    for (let i = a; i <= b; i++){
+        sum += i;
+    }
+    return sum
 };
 
 // Do not edit below this line
