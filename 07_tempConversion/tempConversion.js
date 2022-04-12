6a85d19f6db9c49b164ab30a6d2241abff813d50
ForsakenKappa <jafsadcookie@yandex.ru>
2022-04-13 09:33:07 +1000

    07 is done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
+const ftoc = function(f) {
+
+  let c = Math.round(((f - 32) * 5 / 9 + Number.EPSILON) * 10) / 10;
+  return c
 
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let f = Math.round((c * 9/5 + 32 + Number.EPSILON) * 10) / 10;
+  return f
 };
 
 // Do not edit below this line
