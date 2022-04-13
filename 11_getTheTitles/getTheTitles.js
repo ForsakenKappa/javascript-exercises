9e01e31824d306942c35d0eda815df75ddd83611
ForsakenKappa <jafsadcookie@yandex.ru>
2022-04-13 11:45:16 +1000

    10 is done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    
+    titles = []
 
-};
+    books.forEach(element => {
+        titles.push(element.title)
+    });
 
+    return titles
+}
 // Do not edit below this line
 module.exports = getTheTitles;
