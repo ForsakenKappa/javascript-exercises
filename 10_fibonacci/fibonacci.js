4733c0c756dc3fed1450bc0d8545100948b48f33
ForsakenKappa <jafsadcookie@yandex.ru>
2022-04-13 11:26:27 +1000

    10 is done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    seq = [1,1];
+
+    if (num < 0) return "OOPS"
+
+    for (let i = 0; i < num; i++){
+        seq.push(seq[i] + seq[i+1])
+    }
+
+    return seq[num-1]
+    
 };
 
 // Do not edit below this line
